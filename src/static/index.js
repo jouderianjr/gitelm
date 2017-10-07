74dedc58df754ece4320f47b2b6be90d3958033c
Jouderian Nobre <jouderianjr@gmail.com>
2017-10-07 13:07:01 -0300

    Remove font-awesome files, using CDN now

--- src/static/index.js
+++ src/static/index.js
@@ -1,6 +1,5 @@
 // pull in desired CSS/SASS files
 require( './styles/main.scss' );
-require( './styles/font-awesome/scss/font-awesome.scss' );
 
 // inject bundled Elm app into div#main
 var Elm = require( '../elm/Main' );
